60b6c7b2f0939e3b2e52534a373201fe906eee5e
Liam Mulhall <liammulh@gmail.com>
2024-03-18 18:44:38 -0700

    Add input and output dirs

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,13 @@
+const INPUT_DIR = "_src";
+const OUTPUT_DIR = "_dist";
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("burp.css");
+    eleventyConfig.addPassthroughCopy(`${INPUT_DIR}/CNAME`);
+    eleventyConfig.addPassthroughCopy(`${INPUT_DIR}/burp.css`);
+    return {
+        dir: {
+            input: INPUT_DIR,
+            output: OUTPUT_DIR
+        }
+    }
 };
